53e64fd761c9f1d065d0498001218dcbf908f117
ayushanand16 <ayush.anand.cse21@itbhu.ac.in>
2022-12-10 23:50:23 +0530

    Landing Page Initial Commit

---
+++ components/landing/cards.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+export default function Cards({props}) {
+  console.log(props);
+
+    return (
+      
+    <div className="max-w-sm overflow-hidden shadow-lg mx-auto bg-landing">
+    
+    
+    
+      
+      <Image
+              src={props.url}
+              alt="card_image"
+              width={700}
+              height={30}
+              priority
+      />
+    
+    <div className="mx-6 my-4 border-b border-gray-light ">
+      <div className="font-medium text-base text-gray-darker mb-4">{props.cardname}</div>
+      <p className="font-normal text-gray-dark text-sm mb-2">
+       {props.description}
+      </p>
+      <p className="font-normal text-gray-dark text-sm mb-4">
+       {props.endline}
+      </p>
+    </div>
+    <div className="mx-6 my-4 flex">
+      <div className="flex-grow">
+       
+      </div>
+    
+    </div>
+  </div>
+  
+  
+  
+  
+  
+    )
+  }
+  
+  
