91a998add6fc9aa18279970dcbc0346a75fe9534
Anoushka <95405559+BabyElias@users.noreply.github.com>
2022-12-13 15:45:19 +0530

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		colors: {
+			berry: '#A45693',
+			blush: '#F1E9EE',
+			black: '#000000',
+			mauve: '#C092B2',
+			'pearly purple': '#AD6EA1',
+		},
+
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+				montserrat: ['Montserrat', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 }
