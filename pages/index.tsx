8b9cc3c2aa0afaf220bc91c9e3cca167a8451709
atulyadav745 <106299466+atulyadav745@users.noreply.github.com>
2022-12-14 16:55:39 +0530

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import Navbar from "../components/Navbar/Navbar";
+
 export default function Home() {
 	return (
-		<section className="py-10 h-screen text-5xl text-white font-bold flex justify-center items-center bg-gradient-to-r from-indigo-500 to-pink-500">Hello Study Portal</section>
+		// <section className="py-10 h-screen text-5xl text-white font-bold flex justify-center items-center bg-gradient-to-r from-indigo-500 to-pink-500">Hello Study Portal</section>
+		<Navbar/>
+		
 	)
 }
