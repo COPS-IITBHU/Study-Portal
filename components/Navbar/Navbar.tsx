20d3a941b420792d44a4fd60caa4477b889b84eb
atulyadav745 <106299466+atulyadav745@users.noreply.github.com>
2022-12-14 22:44:24 +0530

    [UPDATED] Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className="">
       <div className="flex items-center font-medium justify-around">
-        <div className="z-50 p-5 md:w-auto w-full flex justify-between">
+        <div className="z-50 p-1 md:w-auto w-full flex justify-between">
           <Image src={Logo} alt="logo" className="md:cursor-pointer h-15 w-14" />
           <div className={`text-3xl md:hidden  duration-500 ${open ? "rotate-0" : "rotate-360"}`} onClick={() => setOpen(!open)}>
             <IonIcon name={`${open ? "close" : "menu"}`}></IonIcon>
@@ -42,11 +42,11 @@ const Navbar = () => {
        
         <ul
           className={`
-        md:hidden  fixed w-full top-0 overflow-y-auto bottom-0 py-24 
+        md:hidden items-center fixed w-full top-0 overflow-y-auto bottom-0 py-24 
         duration-500 ${open ? "left-0" : "left-[-100%]"}
         `}
         >
-          <div className='bg-berry h-full'>
+          <div className='bg-berry text-center justify-between text-blush h-full'>
           <li >
             <Link href="/" className="py-7 px-3 inline-block">
               Home
@@ -64,7 +64,7 @@ const Navbar = () => {
           </li>
          
          
-          <button className=' ml-3 border text-white px-6 py-2 rounded-xl'>
+          <button className='border text-white px-6 py-2 my-7 rounded-xl'>
             Login
         </button>
          
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Navbar from "../components/Navbar/Navbar";
+
 
 export default function Home() {
-	return (
-		// <section className="py-10 h-screen text-5xl text-white font-bold flex justify-center items-center bg-gradient-to-r from-indigo-500 to-pink-500">Hello Study Portal</section>
-		<Navbar/>
+	// return (
+	// 	// <section className="py-10 h-screen text-5xl text-white font-bold flex justify-center items-center bg-gradient-to-r from-indigo-500 to-pink-500">Hello Study Portal</section>
+	// 	<Navbar/>
 		
-	)
-	return <div>Home</div>
+	// )
+	return <div></div>
 }
