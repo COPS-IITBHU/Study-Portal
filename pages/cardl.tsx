c05b4aaeac901516a4825fbf297f49a4ab4ed641
Amit Mishra <amit.mishra.eee21@itbhu.ac.in>
2023-01-13 12:58:20 +0530

    initial

---
+++ pages/cardl.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function cardl() {
+  return (
+    <section className='text-white from-indigo-500 to-pink-500 flex h-screen items-center justify-center bg-gradient-to-r py-10 text-5xl font-bold'>
+			Hey
+		</section>
+  )
+}
+
+export default cardl
