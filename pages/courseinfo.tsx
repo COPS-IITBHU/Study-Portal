05fe35a1270e2b72a67261c56b50b6ffab957d84
Amit Mishra <amit.mishra.eee21@itbhu.ac.in>
2023-01-17 15:13:56 +0530

    all added

--- pages/courseinfo.tsx
+++ pages/courseinfo.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Content from '../components/course_info/Content'
-
+import Book from '../components/course_info/Book'
+import Tut from '../components/course_info/Tut'
+import Past from '../components/course_info/Past'
 const courseinfo = () => {
   return (
     <>
     <Content/>
+    <Book/>
+    <Tut/>
+    <Past/>
+
     </>
   )
 }
